60e1a4f80d72d3ceef6a0be50894b94267b6782e
Blake Barritt <bbarritt1@gmail.com>
2022-07-06 10:48:59 -0700

    add messages from UI

--- app/controllers/chatroom_controller.rb
+++ app/controllers/chatroom_controller.rb
@@ -1,6 +1,9 @@
 class ChatroomController < ApplicationController
+  before_action :require_user
 
   def index
+    @message = Message.new
+    # instance variable for form on index view
     @messages = Message.all
   end
   
